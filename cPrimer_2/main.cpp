b5fd48cab6042a021e236abf20a100480993d3bd
Tanner Davison <Tboydavison@gmail.com>
2024-11-02 02:34:41 -0600

    removed singleton

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -36,36 +36,4 @@ int main() {
 	return 0;
 
 
-	/*std::cin >> data1.item_id >> data1.units_sold >> price;
-	data1.revenue = price * data1.units_sold;
-
-	std::cin >> data2.item_id >> data2.units_sold >> price;
-	data2.revenue = data2.units_sold * price;
-
-
-	if (data1.item_id == data2.item_id) {
-
-		unsigned totalcnt = data1.units_sold + data2.units_sold;
-		double totalrevenue = data1.revenue + data2.revenue;
-
-		std::cout << data1.item_id
-			<< "\n# of items sold: " << totalcnt << " "
-			<< "\ntotal revenue: $" << totalrevenue << " ";
-
-
-		if (totalrevenue != 0) {
-			std::cout << "\nasp: $"
-				<< totalrevenue / totalcnt
-				<< std::endl;
-		}
-		else {
-			std::cout << "\n\t(no sales)" << std::endl;
-		}
-		return 0;
-	}
-	else {
-		std::cerr << "data must refer to the same item_id"
-			<< std::endl;
-	}
-	return 0;*/
 }
