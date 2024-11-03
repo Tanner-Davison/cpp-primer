75a48c93a493bcfc0f6549bdb73f37768a9605af
Tanner Davison <Tboydavison@gmail.com>
2024-11-03 16:17:24 -0700

    pushing

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -22,8 +22,41 @@ int main() {
 			if (n < hexdigits.size()) {
 				cout << hexdigits[n];
 			}
+			else {
+				auto r = (n % 16);
+				auto d = (n / 16);
+				cout << hexdigits[d] << hexdigits[r];
+
+			}
 		}
 		cout << endl;
 	}
-	return 0;
+
+	Sales_data curr_id, next_id;
+
+	double price = 0;
+
+	curr_id.logTime(cout);
+	if (cin >> curr_id) {
+		Sales_data same_id;
+		unsigned asp;
+		int total_item_count = 0;
+		while (cin >> same_id) {
+
+			if (curr_id.item_id == same_id.item_id) {
+
+				curr_id.units_sold += same_id.units_sold;
+				curr_id.revenue += same_id.revenue;
+				total_item_count++;
+			}
+			else {
+				total_item_count++;
+				curr_id.printSalesReport(cout);
+				curr_id = same_id;
+			}
+		}
+		curr_id.printSalesReport(cout);
+		cout << "\n\t Items accounted: " << ++total_item_count;
+		return 0;
+	}
 }
