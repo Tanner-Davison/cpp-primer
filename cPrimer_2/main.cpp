fe0ab67ebb5f0522785aab0004f88dff4c20ae35
Tanner Davison <Tboydavison@gmail.com>
2024-11-03 12:18:22 -0700

    created a hexidecimal converter to read a file of input numbers with values 0-15 and coverts them to their hex equivalent;

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -2,46 +2,28 @@
 using std::cin, std::cout, std::endl;
 #include "Sales_item.h"
 #include "Sales_data.h"
+#include<sstream>
+using std::istringstream;
 #include <string>
-
 using std::toupper;
 using std::string;
 
 
 int main() {
+	const string hexdigits = "0123456789ABCDEF";
+	string result;
+	decltype(result.size()) n;
+	cout << "Enter a series of numbers from 0-15\n"
+		"Separated by ' [spaces] ' Hit [enter] when finished.\n" << endl;
 
-	string str1, str2;
-
-	decltype(str1.size()) str1Cnt = 0; //best practice to ensure length when counting;
-	decltype(str2.size()) str2Cnt = 0;//dont use size_t for strings;
-
-	cin >> str1;
-	cin >> str2;
-
-	for (auto& c : str1) {
-		c = toupper(c);
-		++str1Cnt;
-	}
-	for (auto& c : str2) {
-		c = toupper(c);
-		++str2Cnt;
-	}
-	if (str1Cnt == str2Cnt) {
-		cout << str1 << " count: " << str1Cnt
-			<< " is equal to "
-			<< str2 << " count: " << str2Cnt << endl;
-	}
-	else {
-		if (str1Cnt > str2Cnt) {
-			cout << str1 << " is greater than " << str2 << " by: "
-				<< (str1Cnt - str2Cnt) << " characters" << endl;
+	while (getline(cin, result)) {
+		std::istringstream iss(result);
+		while (iss >> n) {
+			if (n < hexdigits.size()) {
+				cout << hexdigits[n];
+			}
 		}
-		else {
-			cout << str2 << " is greater than " << str1 << " by: "
-				<< (str2Cnt - str1Cnt) << " characters" << endl;
-		}
+		cout << endl;
 	}
 	return 0;
-
-
 }
