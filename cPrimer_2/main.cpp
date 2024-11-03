860952c61eea4e4ff1e34858506dcd1860c73fa6
Tanner Davison <Tboydavison@gmail.com>
2024-11-02 22:57:19 -0600

    String Comparison;

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
+using std::cin, std::cout, std::endl;
 #include "Sales_item.h"
 #include "Sales_data.h"
 #include <string>
 
+using std::toupper;
+using std::string;
 
-using std::cin, std::cout, std::endl;
 
 int main() {
 
-	int i = 5;
-	int x = 2;
+	string str1, str2;
 
-	double b = static_cast<double>(i) / x;
+	decltype(str1.size()) str1Cnt = 0; //best practice to ensure length when counting;
+	decltype(str2.size()) str2Cnt = 0;//dont use size_t for strings;
 
-	std::cout << b;
+	cin >> str1;
+	cin >> str2;
 
+	for (auto& c : str1) {
+		c = toupper(c);
+		++str1Cnt;
+	}
+	for (auto& c : str2) {
+		c = toupper(c);
+		++str2Cnt;
+	}
+	if (str1Cnt == str2Cnt) {
+		cout << str1 << " count: " << str1Cnt
+			<< " is equal to "
+			<< str2 << " count: " << str2Cnt << endl;
+	}
+	else {
+		if (str1Cnt > str2Cnt) {
+			cout << str1 << " is greater than " << str2 << " by: "
+				<< (str1Cnt - str2Cnt) << " characters" << endl;
+		}
+		else {
+			cout << str2 << " is greater than " << str1 << " by: "
+				<< (str2Cnt - str1Cnt) << " characters" << endl;
+		}
+	}
 	return 0;
 
 
