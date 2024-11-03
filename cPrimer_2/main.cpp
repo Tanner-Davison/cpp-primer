ee82be87c57de92427dfe7c5f99fdf11a82b3830
Tanner Davison <Tboydavison@gmail.com>
2024-11-03 16:59:28 -0700

    doubled up

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -7,32 +7,42 @@ using std::istringstream;
 #include <string>
 using std::toupper;
 using std::string;
-
+#include <fstream>
 
 using  std::cout, std::endl, std::cin;
+
+void printHex(int n) {
+	const string hexdigits = "0123456789ABCDEF";
+	if (n >= 16) {
+		printHex(n / 16);  // recursively handle larger numbers
+	}
+	cout << hexdigits[n % 16];
+}
 
 int main() {
 	const string hexdigits = "0123456789ABCDEF";
 	string result;
 	decltype(result.size()) n;
-	cout << "Enter a series of numbers from 0-15\n"
+	cout << "Enter a series test of numbers from 0-15\n"
 		"Separated by ' [spaces] ' Hit [enter] when finished.\n" << endl;
-
-	while (getline(cin, result)) {
+	std::ifstream inFile("hextest.txt");
+	if (!inFile) {
+		std::cout << "Error opening file" << std::endl;
+		return 1;
+	}
+	while (getline(inFile, result)) {
 		std::istringstream iss(result);
 		while (iss >> n) {
 			if (n < hexdigits.size()) {
 				cout << hexdigits[n];
 			}
 			else {
-				auto r = (n % 16);
-				auto d = (n / 16);
-				cout << hexdigits[d] << hexdigits[r];
-
+				printHex(n);
 			}
 		}
 		cout << endl;
 	}
+	inFile.close();
 
 	Sales_data curr_id, next_id;
 
