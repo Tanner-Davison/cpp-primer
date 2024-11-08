09b6338cbe06428b0c1c8210e2a5df16eca30c21
Tanner Davison <Tboydavison@gmail.com>
2024-11-08 00:12:31 -0700

    stashing

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -10,9 +10,11 @@ int main() {
 	int arr[sz]{};
 
 	int* ptrArr = arr;	//arr[0];
+
 	int* c = &arr[10];	//points to 1 past the last element;
 
 	int count = 0;
+
 	for (int* b = arr; b != c; ++b) {
 
 		*b = ++count;	//increments count by 1 adding value to b;
