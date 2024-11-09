14b7a36c809a4f1bf56161f29e97985af3b42c07
Tanner Davison <Tboydavison@gmail.com>
2024-11-09 15:44:57 -0700

    added c style string references

--- cPrimer_2/main.cpp
+++ cPrimer_2/main.cpp
@@ -22,79 +22,24 @@ void upfirst(string& str) {
 	}
 }
 
-struct Rep {
-	string company, title, rep;
-	vector<string> garbage;
-	Rep(vector<string>& reps) {
-		if (reps.size() >= 3) {
-			company = reps[0];
-			title = reps[1];
-			rep = reps[2];
-		}
-		if (reps.size() > 3) {
-			garbage.assign(reps.begin() + 3, reps.end());
-			for (auto& word : garbage) {
-				up(word);
-			}
-		}
-		up(title);
-		upfirst(company);
-		upfirst(rep);
-	}
-	friend ostream& operator <<(ostream& output, const Rep& rep) {
-		output << rep.title << " " << rep.company << " " << rep.rep << "\n";
-		if (rep.garbage.size() > 0) {
-			output << "\n\tGarbage Found: [";
-			for (auto g : rep.garbage) {
-				output << g << ",";
-			}
-			if (!rep.garbage.empty()) {
-				output << rep.garbage.back();
-			}
-			output << "] " << "\n";
-		}
-
-		return output;
-	}
-};
-
 int main() {
 
-	fstream inFile("input_add_item.txt");
-	if (!inFile) cout << "No input file found!" << endl;
-	string title, company, rep;
-	string line;
+	char ca[] = { 'c','h','a','r','\0' };
+	char ca2[] = { 'a','r','r','a','y','\0' };
 
-	vector<Rep> allReps;
+	string combined;
+	combined += ca;
+	combined += " ";
+	combined += ca2;
 
-	while (getline(inFile, line)) {
-		vector<string> singleRep;
-		string word;
-		//go through each line assaigning word to valid character and push to array;
-		for (auto linep = line.begin(); linep != line.end(); ++linep) {
-			if (!isspace(*linep)) {
-				word += *linep;
-			}
-			else if (!word.empty()) {
-				singleRep.push_back(word);
-				word.clear();
-			}
-		}
-		if (!word.empty()) {
-			singleRep.push_back(word);
-			word.clear();
-		}
-		if (!singleRep.empty()) {
-			Rep newRep = singleRep;
-			allReps.push_back(newRep);
-		}
+	auto* p = &combined;
+	while (p)
+	{
+		cout << *p;
+		++p;
 	}
 
-	for (auto r : allReps) {
-		cout << r << endl;
-	}
-
-
+	cout << endl;
 
 	return 0;
 }
