461757caa62dc73d3746ceb9e1aabe0556d2ea86
Tanner Davison <Tboydavison@gmail.com>
2024-12-31 12:45:00 -0700

    Commiting clang-format options

--- cppPrimer/main.cpp
+++ cppPrimer/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
-
 using namespace std;
+
 int main() {
 
   int num1, num2;
   while (cin >> num1 >> num2) {
     try {
+      
       cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
       if (num1 + num2 <= 0) {
         throw runtime_error("Addition must be greater than 0");
