73f462d263965db83f763991add3c320fc70e885
Tanner Davison <Tboydavison@gmail.com>
2024-12-30 18:33:18 -0700

    Added features

--- cppPrimer/chapters/five/main.cpp
+++ cppPrimer/chapters/five/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
   string input1;
-  while (cin >> input1 && !input1.empty() && input1 != "n") {
+  while (getline(std::cin, input1) && !input1.empty() && input1 != "n") {
     switch (input1[0]) {
     case '-':
       for (auto it = input1.begin() + 1; it != input1.end(); ++it) {
-        if (*it == ' ')
+        if (*it == ' ') {
           break;
+        }
         cout << *it;
       }
+      break;
     case 't':
       for (auto it = input1.begin() + 1; it != input1.end(); ++it) {
         if (*it == 'x') {
           break;
         }
+        cout << *it;
       }
+      break;
     }
+    cout << endl;
   }
   return 0;
 }
