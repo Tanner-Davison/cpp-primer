7e5b522022ab5fac25602977a02798ee16ef938a
Tanner Davison <Tboydavison@gmail.com>
2024-12-31 22:33:50 -0700

    added exception handeling and multiple cases

--- cppPrimer/chapters/five/main.cpp
+++ cppPrimer/chapters/five/main.cpp
@@ -6,9 +6,23 @@
 // Test for duplicates words in input stream
 using namespace std;
 int main() {
-  string line;
+  string line = "";
   string current_word = "";
   vector<string> duplicates;
+  cout << "Uppercase / Any type u or a\n";
+  char resp;
+  string input;
+  try {
+    getline(cin, input);
+    resp = input[0];
+    if (!resp || (resp != 'a' && resp != 'u')) {
+      throw runtime_error("Must set boundry!");
+    } else {
+      cout << "\nBoundry set!\n";
+    }
+  } catch (runtime_error err) {
+    cout << err.what() << endl;
+  }
   while (getline(cin, line) && !line.empty()) {
     stringstream s(line);
     string prev;
@@ -16,7 +30,12 @@ int main() {
       if (!current_word.empty() && current_word != prev) {
         prev = current_word;
       } else {
-        if (std::isupper(current_word[0])) {
+        switch (resp) {
+        case 'u':
+          if (!std::isupper(current_word[0]) && current_word == prev) {
+            break;
+          }
+        default:
           duplicates.emplace_back(current_word);
           prev = current_word;
         }
