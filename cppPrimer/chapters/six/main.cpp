41b3d46176ccde7605c3f2790772078436c159bd
Tanner Davison <Tboydavison@gmail.com>
2024-12-31 18:02:33 -0700

    Commiting stage

--- cppPrimer/chapters/six/main.cpp
+++ cppPrimer/chapters/six/main.cpp
@@ -7,11 +7,13 @@ int fact(int num) {
   }
   return ret;
 }
-
+unsigned int abs(unsigned int num) { return num; }
 int main() { // --< start
-  int number{5};
-  int factof = fact(number);
-  cout << factof << endl;
-
+  int factOfFive = fact(5);
+  int factOfFour = fact(4);
+  int total = factOfFive * factOfFour;
+  cout << total;
+  int myabs = -100;
+  cout << abs(myabs) << endl;
   return 0;
 }
