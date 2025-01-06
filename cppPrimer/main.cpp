0083fcfe74d85ba1da060321e5dbe50ad1df0ef8
Tanner Davison <Tboydavison@gmail.com>
2025-01-05 22:30:25 -0700

    UPdated Primer from page 217

--- cppPrimer/main.cpp
+++ cppPrimer/main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 using namespace std;
+void print(const int arr[], const int &len) {
+  for (int i = 0; i < len; ++i) {
+    cout << arr[i] << endl;
+  }
+}
 
 int main() {
   float num, num2;
+  int numP[19] = {1};
+  const int numPLength = 19;
+  for (int i = 0; i < 19; ++i) {
+    numP[i] = i;
+  }
+  print(numP, numPLength);
   while (cin >> num >> num2) {
     try {
       if (num2 == 0)
         throw runtime_error("ERROR! -> Attempted to divide by 0");
       cout << num << " / " << num2 << " = " << (num / num2) << endl;
-    } catch (runtime_error err) {
+    } catch (runtime_error &err) {
       cout << err.what() << "\nTry again? y or n\n";
-      char rs;
-      cin >> rs;
-      if (rs == 'n' || !rs) {
+      char rsp;
+      cin >> rsp;
+      if (rsp == 'n' || !rsp) {
         break;
       }
     }
   }
+  cout << "Working" << endl;
+
   return 0;
 }
