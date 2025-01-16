397093d205f02a508a2cd40725f3473e590199de
Tanner Davison <Tboydavison@gmail.com>
2025-01-15 22:48:44 -0700

    function pointers

--- cppPrimer/main.cpp
+++ cppPrimer/main.cpp
@@ -5,9 +5,16 @@ typedef int arrT[10];
 arrT *func(int i);
 // form of a func that returns an array
 //  int (*p2)[10] = &arr;
-int main(int argc, char *argv[]) {
-  typedef int arrT[10];
-  int(*p2)[10];
+int (*func(int))[10] {
+  static int arrS[10];
+  for (int i = 0; i < 10; ++i) {
+    arrS[i] = i;
+  }
+  return &arrS;
+};
 
+int main() {
+  int(*testPtr)[10] = func(5);
+  std::cout << (*testPtr)[3] << std::endl;
   return 0;
 }
