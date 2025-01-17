02cce9b6b0355e52c75d581e7d9080769ce4ef31
Tanner Davison <Tboydavison@gmail.com>
2025-01-16 23:15:57 -0700

    practicing throwing non polymorphic types

--- cppPrimer/main.cpp
+++ cppPrimer/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
-
-typedef int arrT[10];
-
-arrT *func(int i);
-// form of a func that returns an array
-//  int (*p2)[10] = &arr;
-int (*func(int))[10] {
-  static int arrS[10];
-  for (int i = 0; i < 10; ++i) {
-    arrS[i] = i;
+typedef int arrP[10];
+int (*func(int p))[10] {
+  static arrP arrP;
+  try {
+    if (p > 0) {
+      for (int i = 0; i < p; ++i) {
+        arrP[i] = i;
+        std::cout << arrP[i] << std::endl;
+      }
+      return &arrP;
+    } else {
+      throw std::runtime_error("function has no valid parameters");
+    }
+  } catch (std::runtime_error &err) {
+    std::cout << err.what() << std::endl;
+    return nullptr;
   }
-  return &arrS;
-};
-
+  return nullptr;
+}
 int main() {
-  int(*testPtr)[10] = func(5);
-  arrT *tenInts = func(10);
-  std::cout << (*tenInts)[9] << std::endl;
+  int(*arr)[10] = func(0);
 
   return 0;
 }
