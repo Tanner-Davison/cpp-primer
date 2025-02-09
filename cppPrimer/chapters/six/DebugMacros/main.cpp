00eb4fa6d400d3c8d70f9fdfc584658c44c54c5e
Tanner Davison <Tboydavison@gmail.com>
2025-02-08 21:58:40 -0700

    inline constexpr

--- cppPrimer/chapters/six/DebugMacros/main.cpp
+++ cppPrimer/chapters/six/DebugMacros/main.cpp
@@ -5,20 +5,22 @@
 void print(const int ia[], size_t size) {
 #ifndef NDEBUG
   std::cerr << std::source_location::current().function_name()
-            << " : array size is " << size << "\n";
+            << " : array size is :" << size << " @ line : " << __LINE__
+            << " Date:" << __DATE__ << " \nTime: " << __TIME__ << std::endl;
 #endif
   for (size_t i = 0; i < size; i++) {
     std::cout << ia[i] << " ";
   }
   std::cout << std::endl;
 }
-inline std::string isShorter(const std::string &str, const std::string &str2) {
+inline constexpr std::string isShorter(const std::string &str,
+                                       const std::string &str2) {
   return str.size() > str2.size() ? str2 : str;
 }
 
-int main(int c, char *[]) {
+int main() {
   std::string name = "Tanner";
-  std::string name2 = "Hum";
+  std::string name2 = "Short";
   std::cout << isShorter(name, name2) << std::endl;
   int arr[] = {1, 2, 3, 4, 5};
   print(arr, 5);
