8d3ef6055e43f01a911bf4b482ae6bc8c533125b
Tanner Davison <Tboydavison@gmail.com>
2025-02-08 20:48:19 -0700

    saving

--- cppPrimer/chapters/six/DebugMacros/main.cpp
+++ cppPrimer/chapters/six/DebugMacros/main.cpp
@@ -12,8 +12,14 @@ void print(const int ia[], size_t size) {
   }
   std::cout << std::endl;
 }
+inline std::string isShorter(const std::string &str, const std::string &str2) {
+  return str.size() > str2.size() ? str2 : str;
+}
 
-int main() {
+int main(int c, char *[]) {
+  std::string name = "Tanner";
+  std::string name2 = "Hum";
+  std::cout << isShorter(name, name2) << std::endl;
   int arr[] = {1, 2, 3, 4, 5};
   print(arr, 5);
   return 0;
