3341c98e1f40a7fac5885cbf99bc560d4e8c5b31
Tanner Davison <Tboydavison@gmail.com>
2025-02-08 22:18:35 -0700

    Prepocessing Test

--- cppPrimer/chapters/six/RecursiveCalls/main.cpp
+++ cppPrimer/chapters/six/RecursiveCalls/main.cpp
@@ -1,14 +1,18 @@
+#include <cassert>
 #include <iostream>
+#define DEBUG
 
 void readArr(int nums[], int size) {
-  static int id = size;
-  std::cout << nums[size] << std::endl;
-  if (id != 0) {
-    readArr(nums, (--id));
+#ifdef DEBUG
+  std::cerr << " arr size : " << size << "\n@ Line # " << __LINE__
+            << "\nDate: " << __DATE__ << "\nTime: " << __TIME__ << std::endl;
+#endif
+  if (size > 0) {
+    std::cout << nums[size - 1] << std::endl;
+    readArr(nums, size - 1);
   }
 }
 int main() {
-  std::cout << "Program Starts here -->" << std::endl;
   int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   readArr(numbers, 10);
   return 0;
