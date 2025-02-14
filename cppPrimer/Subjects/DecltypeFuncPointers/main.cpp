ae35464618c329a8363e38e58ab6b2063459cb0b
Tanner Davison <Tboydavison@gmail.com>
2025-02-13 19:02:44 -0700

    Finished Chapter six Function Pointers
    Finished Chapter Six For Students and Have a quiz section with notes on
    function pointers

--- cppPrimer/Subjects/DecltypeFuncPointers/main.cpp
+++ cppPrimer/Subjects/DecltypeFuncPointers/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+// Section 6.7
 // Create 4 functions Add, Subtract, Multiply, Divide
 int add(int num1, int num2) { return (num1 + num2); };
 int subtract(int num1, int num2) { return (num1 - num2); };
@@ -8,8 +9,11 @@ int multiply(int num1, int num2) { return (num1 * num2); };
 int divide(int num1, int num2) { return (num1 / num2); };
 
 // typedef int (*mathops)(int, int);
-
 using mathops = int (*)(int, int);
+
+int callback(int num1, int num2, int (*func_cb)(int, int)) {
+  return func_cb(num1, num2);
+}
 
 int main() {
 
@@ -17,7 +21,8 @@ int main() {
   int num1 = 10;
   int num2 = 20;
   for (std::vector<mathops>::iterator it = ops.begin(); it != ops.end(); ++it) {
-    std::cout << "total: " << (*it)(num2, num1) << std::endl;
+    std::cout << "Total: " << (*it)(num2, num1) << std::endl;
   }
+  std::cout << "Callback() = " << callback(num1, num2, ops[0]);
   return 0;
 }
