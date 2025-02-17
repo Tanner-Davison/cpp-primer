830efa8d90e0d998f7af3f91c744482bc80c071d
Tanner Davison <Tboydavison@gmail.com>
2025-02-16 22:51:44 -0700

    Course Created for Teusday

--- cppPrimer/chapters/7.3/Screen.hpp
+++ cppPrimer/chapters/7.3/Screen.hpp
@@ -15,11 +15,19 @@ public:
   char get() const;
   Screen &move(pos r, pos c);
   void some_member() const;
+
   // Inline member functions
   inline char get(pos ht, pos wd) const;
   inline Screen &set(char);
   inline Screen &set(pos, pos, char);
+  inline Screen &display(std::ostream &os);
+  inline const Screen &display(std::ostream &os) const;
 
+  // Private Member Functions
+private:
+  void do_display(std::ostream &os) const { os << contents; };
+
+  // private data members
 private:
   mutable size_t access_ctr; // May change even a const object
   pos cursor = 0;
@@ -32,12 +40,23 @@ inline char Screen::get(pos r, pos c) const {
   pos row = r * width;
   return contents[row + c];
 }
+// set
 inline Screen &Screen::set(char c) {
   contents[cursor] = c;
   return *this;
 };
-
+// set
 inline Screen &Screen::set(pos r, pos col, char c) {
   contents[r * width + col] = c;
+  return *this;
+};
+// display
+inline Screen &Screen::display(std::ostream &os) {
+  do_display(os);
+  return *this;
+};
+// const display
+inline const Screen &Screen::display(std::ostream &os) const {
+  do_display(os);
   return *this;
 };
