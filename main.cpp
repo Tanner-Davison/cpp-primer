16976b7a2c9010a8b63fbac1023841f2dc12ca48
Tanner Davison <tanner.davison95@gmail.com>
2025-02-20 01:51:00 -0700

    fixed compiler flags and ready for lesson overview

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+// Copyright (c) 2025 Tanner Davison. All rights reserved
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  * of this software and associated documentation files (the "Software"), to
+//  deal * in the Software without restriction, including without limitation the
+//  rights * to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell * copies of the Software, and to permit persons to whom the
+//  Software is * furnished to do so, subject to the following conditions: * * -
+//  This copyright notice and permission notice shall be included in all *
+//  copies or substantial portions of the Software. * * THE SOFTWARE IS PROVIDED
+//  "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR * IMPLIED, INCLUDING BUT
+//  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, * FITNESS FOR A PARTICULAR
+//  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE * AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER * LIABILITY, WHETHER IN AN
+//  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, * OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN * THE SOFTWARE.
+
 #include <algorithm>
 #include <cctype>
 #include <iostream>
@@ -10,7 +27,7 @@ int multiply(int a, int b) { return (a * b); }
 
 int calculate(int a, int b, int (*operation)(int, int)) {
   return operation(a, b);
-};
+}
 // see chapters/7
 int (*getOperation(char op))(int, int) {
   switch (op) {
@@ -22,8 +39,8 @@ int (*getOperation(char op))(int, int) {
     return &multiply;
   default:
     return nullptr;
-  };
-};
+  }
+}
 
 bool (*pf)(int &num, int &num2) = [](int &num, int &num2) {
   return num == num2;
