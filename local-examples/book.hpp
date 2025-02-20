6fc689c0f827a6b5b8d38376961533b406a6998d
Tanner Davison <Tboydavison@gmail.com>
2025-02-19 21:20:29 -0700

    implementing Books Library for lesson plan

--- local-examples/book.hpp
+++ local-examples/book.hpp
@@ -11,12 +11,17 @@ public:
 
   friend bool read(std::istream &, Book &);
   // member functions
+  std::string get_auth(Book &) const;
+  std::string get_book_name(Book &) const;
+  std::string get_published_date(Book &) const;
+
   // friend
   friend std::istream &operator>>(std::istream &, Book &);
   friend void print(std::ostream &, Book &);
   friend std::ostream &operator<<(std::ostream &, const Book &);
 
 private:
+  std::string book_id;
   std::string author;
   std::string book_name;
   std::string total_pages;
