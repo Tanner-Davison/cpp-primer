6084198de583e1c874f4a76681ba9ce8986f6a43
Tanner Davison <Tboydavison@gmail.com>
2025-02-20 01:58:44 -0700

    added copyright claims

--- Sales_item.h
+++ Sales_item.h
@@ -1,46 +1,29 @@
 #pragma once
 
 /*
- * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
- * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
- * copyright and warranty notices given in that book:
- *
- * "Copyright (c) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E. Moo."
- *
- *
- * "The authors and publisher have taken care in the preparation of this book,
- * but make no expressed or implied warranty of any kind and assume no
- * responsibility for errors or omissions. No liability is assumed for
- * incidental or consequential damages in connection with or arising out of the
- * use of the information or programs contained herein."
+ * // Copyright (c) 2025 Tanner Davison. All rights reserved
  *
  * Permission is granted for this code to be used for educational purposes in
- * association with the book, given proper citation if and when posted or
- * reproduced.Any commercial use of this code requires the explicit written
- * permission of the publisher, Addison-Wesley Professional, a division of
- * Pearson Education, Inc. Send your request for permission, stating clearly
+ * association, given proper citation if and when posted or
+ * reproduced. Any commercial use of this code requires the explicit written
+ * permission of the publisher, Tanner Davison , a division of
+ * Send your request for permission, stating clearly
  * what code you would like to use, and in what specific way, to the following
  * address:
- *
- *     Pearson Education, Inc.
- *     Rights and Permissions Department
- *     One Lake Street
- *     Upper Saddle River, NJ  07458
- *     Fax: (201) 236-3290
+ *     The Davison Boys, llc.
+ *     227 w thistle dr
+ *     Ut, 84045
+ *     email: tanner.davison@gmail.com
  */
 
 /* This file defines the Sales_item class used in chapter 1.
  * The code used in this file will be explained in
  * Chapter 7 (Classes) and Chapter 14 (Overloaded Operators)
- * Readers shouldn't try to understand the code in this file
- * until they have read those chapters.
  */
 
 #ifndef SALESITEM_H
 // we're here only if SALESITEM_H has not yet been defined
 #define SALESITEM_H
-
-#include "Version_test.h"
 
 // Definition of Sales_item class and related functions goes here
 #include <iomanip>
@@ -48,16 +31,12 @@
 #include <string>
 
 class Sales_item {
-  // these declarations are explained section 7.2.1, p. 270
-  // and in chapter 14, pages 557, 558, 561
   friend std::istream &operator>>(std::istream &, Sales_item &);
   friend std::ostream &operator<<(std::ostream &, const Sales_item &);
   friend bool operator<(const Sales_item &, const Sales_item &);
   friend bool operator==(const Sales_item &, const Sales_item &);
 
 public:
-  // constructors are explained in section 7.1.4, pages 262 - 265
-  // default constructor needed to initialize members of built-in type
 #if defined(IN_CLASS_INITS) && defined(DEFAULT_FCNS)
   Sales_item() = default;
 #else
