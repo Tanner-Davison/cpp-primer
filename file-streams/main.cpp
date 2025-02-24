e9ea454f97d0c98cc791bea61b3761e102edf97c
Tanner Davison <Tboydavison@gmail.com>
2025-02-23 19:47:04 -0700

    file streams presentation complete

--- file-streams/main.cpp
+++ file-streams/main.cpp
@@ -1,31 +1,46 @@
 #include "../debug-class-example/DebugEx.hpp"
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-void process_input(std::istream &in, int &valp) {
-  in >> valp;
-  std::cout << "value: " << valp << std::endl;
+void process_input(std::istream &in, std::string &valp) {
+  while (std::getline(in, valp)) {
+    if (!valp.empty()) {
+      std::cout << "\n" << valp << std::endl;
+    }
+  };
 }
 
 int main() {
+  std::fstream inFile("./testing.txt");
+  if (!inFile)
+    throw std::runtime_error("No input file found");
   Debug io_db(false, true, false);
-  int val;
+  std::string val;
 
   while (true) {
-    auto old_state = std::cin.rdstate();
-    std::cin.clear();
+    auto old_state = inFile.rdstate();
+    inFile.clear(inFile.rdstate() & ~inFile.failbit & ~inFile.badbit);
 
-    process_input(std::cin, val);
+    process_input(inFile, val);
 
-    if (!std::cin.good()) {
-      if (io_db.any()) {
-        std::cerr
-            << "Failed to read input from std::cin. Expected type: ( INT )\n"
-            << "error state: " << std::cin.rdstate() << std::endl;
-        break;
+    if (!inFile.good()) {
+      if ((inFile.rdstate() == std::ios::eofbit) | std::ios::failbit) {
+        if (io_db.any()) {
+          std::cerr << "- end of file -" << std::endl;
+        }
+      } else {
+        if (io_db.any()) {
+          std::cerr
+              << "Failed to read input from std::cin. Expected type: ( INT )\n"
+              << "\terror state: " << inFile.rdstate() << std::endl;
+        }
       }
+      break;
     }
 
-    std::cin.setstate(old_state);
+    inFile.setstate(old_state);
   }
 
   return 0;
