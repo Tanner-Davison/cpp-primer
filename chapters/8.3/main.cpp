eb8e886c6953ab2f872ed1ac254426f888ad72d9
Tanner Davison <Tboydavison@gmail.com>
2025-02-27 23:59:20 -0700

    fixed formatting

--- chapters/8.3/main.cpp
+++ chapters/8.3/main.cpp
@@ -6,7 +6,18 @@
 struct Person {
   std::string name;
   std::vector<std::string> phones;
-  int get_phone_count() { return phones.size(); };
+  std::vector<std::string> bad_numbers;
+
+  unsigned int get_phone_count() {
+    int psize = phones.size();
+    int bsize = bad_numbers.size();
+    if (psize > bsize) {
+      return (psize - bsize);
+    } else {
+      return (bsize - psize);
+    }
+  };
+  int get_invalid_count() { return (bad_numbers.size()); };
 };
 bool valid(const std::string &nums) {
   if (nums.empty())
@@ -53,19 +64,23 @@ int main() {
 
     for (auto &nums : person.phones) {
       if (!valid(nums)) {
-        badNums << " " << nums;
+        badNums << " " << nums << ",";
       } else
         formatted << " " << formatPhoneNumber(nums);
     }
     if (badNums.str().empty()) {
-      out << "\nName: " << person.name << " id: " << ++count
-          << "\n\tPhones #'s: ";
-      out << formatted.str() << "\n\tcount: " << "[" << person.get_phone_count()
-          << "]"
-          << "\n\n\n";
+      out << "[id: " << ++count << "]\n"
+          << "Name: " << person.name << "\n\t[" << person.get_phone_count()
+          << "] phone #'s: \n\t[" << formatted.str() << "]\n\n";
     } else {
-      std::cerr << "Input Error! " << person.name << " invalid number(s) "
+      std::cerr << "input error! " << person.name << " invalid number(s) "
                 << badNums.str() << std::endl;
+      person.bad_numbers.push_back(badNums.str());
+      out << "\nName: " << person.name << " id: " << ++count << "\n["
+          << person.get_phone_count()
+          << "] Phone Number(s): " << formatted.str() << "\n\t"
+          << "\n\t(" << person.get_invalid_count()
+          << ") invalid numbers: \n\t[ " << badNums.str() << " ]\n\n\n";
     }
   }
   return 0;
