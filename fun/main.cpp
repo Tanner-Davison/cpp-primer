dc292d60d2628e2f65e7450922a42ab9a8449e2b
Tanner Davison <Tboydavison@gmail.com>
2025-03-10 23:02:00 -0600

    saving

--- fun/main.cpp
+++ fun/main.cpp
@@ -3,9 +3,9 @@
 int main() {
   // Initialize ncurses
   initscr();
-  cbreak();              // Line buffering disabled
-  noecho();              // Don't echo keypresses
-  keypad(stdscr, TRUE);  // Enable function keys
+  cbreak();             // Line buffering disabled
+  noecho();             // Don't echo keypresses
+  keypad(stdscr, TRUE); // Enable function keys
 
   // Clear the screen and print message
   clear();
