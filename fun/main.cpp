69415e98df21bbd7c240fd3ff8d271ff0cc9479e
Tanner Davison <Tboydavison@gmail.com>
2025-03-16 00:37:36 -0600

    First time using the spaceship operator <==>

--- fun/main.cpp
+++ fun/main.cpp
@@ -3,24 +3,9 @@
 #include <list>
 #include <vector>
 
-void remove_odds(std::list<int> &strs) {
-  if (!strs.empty()) {
-    auto it = strs.begin();
-    while (it != strs.end()) {
-      if (*it % 2) {
-        it = strs.erase(it);
-      } else {
-        ++it;
-      }
-    }
-  }
-  for (const auto &num : strs) {
-    std::cout << num << ", ";
-  }
-  std::cout << std::endl;
-}
 int main() {
   std::list<int> my_strings{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  remove_odds(my_strings);
+  auto erased = std::erase_if(my_strings, [](int num) { return num % 2; });
+  std::cout << erased << std::endl;
   return 0;
 }
---
+++ testing/main.cpp
@@ -0,0 +1,29 @@
+#include <compare>
+#include <iostream>
+
+struct Point {
+  int x, y;
+
+  // Define three-way comparison
+  auto operator<=>(const Point &other) const {
+    // First compare x values
+    if (auto cmp = x <=> other.x; cmp != 0) {
+      return cmp;
+    }
+    // If x values are equal, compare y values
+    return y <=> other.y;
+  }
+
+  // The compiler will automatically generate ==, !=, <, <=, >, >= from <=>
+};
+
+int main() {
+  Point p1{1, 2};
+  Point p2{1, 3};
+
+  if (p1 < p2) {
+    std::cout << "p1 is less than p2" << std::endl;
+  }
+
+  return 0;
+}
