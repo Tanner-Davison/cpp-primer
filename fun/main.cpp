57b081fcfeb73bf00998777bbb07cbfcc7be620b
Tanner Davison <Tboydavison@gmail.com>
2025-03-16 23:24:35 -0600

    finished quiz 9.29 in 9.3.4

--- fun/main.cpp
+++ fun/main.cpp
@@ -1,7 +1,39 @@
 #include <deque>
+#include <forward_list>
 #include <iostream>
 #include <list>
 #include <vector>
+
+// My result for quiz 9.3.4 / #9.28 page 352
+void find_insert_string(std::forward_list<std::string> &flist,
+                        const std::string &str1, const std::string &str2) {
+  auto prev = flist.before_begin();
+  auto curr = flist.begin();
+  if (!flist.empty() && !str1.empty() && !str2.empty()) {
+    bool found = false;
+    while (curr != flist.end() && !found) {
+      if (*curr == str1) {
+        prev = curr;
+        curr = flist.insert_after(curr, str2);
+        found = true;
+      } else {
+        prev = curr;
+        ++curr;
+      }
+    }
+    if (found == false) {
+      flist.insert_after(prev, str2);
+      std::cout << "\nInserted string at the end of forward list." << std::endl;
+    };
+  } else {
+    if (!str1.empty() && !str2.empty()) {
+      std::cout << "\nList is empty! adding first element.\n" << std::endl;
+      flist.insert_after(flist.before_begin(), str2);
+    } else {
+      std::cout << "\nstr1 or str2 is empty!\n" << std::endl;
+    }
+  }
+};
 
 int main() {
   int ia[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -23,6 +55,7 @@ int main() {
   while (listIt != ilist.end()) {
     if (*listIt % 2 == 0) {
       listIt = ilist.erase(listIt);
+      ++listIt;
     } else {
       ++listIt;
     }
@@ -38,9 +71,37 @@ int main() {
     std::cout << num << ",";
   }
   std::cout << std::endl;
+  std::forward_list<int> flist = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  auto prev = flist.before_begin();
+  auto curr = flist.begin();
+  while (curr != flist.end()) {
+    if (*curr % 2 == 0) {
+      curr = flist.erase_after(prev);
+    } else {
+      prev = curr;
+      ++curr;
+    }
+  }
+  std::cout << "\nForward List: ";
+  for (const auto &num : flist) {
+    std::cout << num << ", ";
+  }
+  std::cout << std::endl;
   std::list<int> my_strings{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
   auto erased = std::erase_if(my_strings, [](int num) { return num % 2; });
 
   std::cout << erased << " numbers removed from (erase_if)" << std::endl;
+
+  // 9.2.8
+  std::forward_list<std::string> strList = {"Alpha", "Bravo", "Charlie"};
+  std::string fstr = "Charlie";
+  std::string input_str = "Delta";
+  find_insert_string(strList, fstr, input_str);
+  std::cout << "Forward list function: " << std::endl;
+  for (const auto &str : strList) {
+    std::cout << str << ", ";
+  }
+  std::cout << std::endl;
   return 0;
 }
