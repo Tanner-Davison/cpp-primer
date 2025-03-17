c5d34e9c1298499cd51afc92dfb42b6196469e48
Tanner Davison <Tboydavison@gmail.com>
2025-03-16 22:08:05 -0600

    quiz completed 9.3.3

--- fun/main.cpp
+++ fun/main.cpp
@@ -4,8 +4,43 @@
 #include <vector>
 
 int main() {
+  int ia[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  std::vector<int> ivec;
+  std::list<int> ilist;
+  for (int i = 0; i < 11; ++i) {
+    ivec.push_back(ia[i]);
+    ilist.push_back(ia[i]);
+  }
+  auto vecIt = ivec.begin();
+  auto listIt = ilist.begin();
+  while (vecIt != ivec.end()) {
+    if (*vecIt % 2) {
+      vecIt = ivec.erase(vecIt);
+    } else {
+      ++vecIt;
+    }
+  }
+  while (listIt != ilist.end()) {
+    if (*listIt % 2 == 0) {
+      listIt = ilist.erase(listIt);
+    } else {
+      ++listIt;
+    }
+  }
+
+  std::cout << "Evens: ";
+  for (const auto &num : ivec) {
+    std::cout << num << ",";
+  }
+  std::cout << std::endl;
+  std::cout << "Odds: ";
+  for (const auto &num : ilist) {
+    std::cout << num << ",";
+  }
+  std::cout << std::endl;
   std::list<int> my_strings{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   auto erased = std::erase_if(my_strings, [](int num) { return num % 2; });
-  std::cout << erased << std::endl;
+
+  std::cout << erased << " numbers removed from (erase_if)" << std::endl;
   return 0;
 }
