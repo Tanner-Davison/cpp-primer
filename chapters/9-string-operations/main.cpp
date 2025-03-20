2828d906de40c66624e2c11abef8a09435d732b3
Tanner Davison <Tboydavison@gmail.com>
2025-03-19 22:09:38 -0600

    seperated concerns

--- chapters/9-string-operations/main.cpp
+++ chapters/9-string-operations/main.cpp
@@ -6,17 +6,22 @@
 // has an out of bounds in case the loop trys to access elements in vector which
 // do not exist example of how .at() throws and how std::out_of_range catches
 //
-int main(int argc, char *argv[]) {
-  std::vector<std::string> args(argv, argv + argc);
+
+void print_args(std::vector<std::string> &args) {
   try {
     std::cout << "Printing Args: \n";
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 0; i < static_cast<int>(args.size()); ++i) {
       std::cout << std::setw(10) << args.at(i) << std::endl;
     }
 
   } catch (std::out_of_range &e) {
     std::cout << e.what() << std::endl;
   }
+}
+
+int main(int argc, char *argv[]) {
+  std::vector<std::string> mainArgs(argv, argv + argc);
+  print_args(mainArgs);
 
   return 0;
 }
