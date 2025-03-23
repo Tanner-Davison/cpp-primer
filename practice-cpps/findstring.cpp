c73967d59d16a462645ddf975170ba52a42e98d6
Tanner Davison <Tboydavison@gmail.com>
2025-03-22 23:06:50 -0600

    made a function to find alpha numberic characters vs alphabetical

---
+++ practice-cpps/findstring.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "No input read file found" << std::endl;
+  }
+}
--- practice-cpps/keepline.cpp
+++ practice-cpps/keepline.cpp
@@ -1,13 +1,43 @@
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <vector>
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
-    return 1;
+void find_nums(const std::string &numbers, const std::string &line) {
+  std::list<char> nums;
+  std::list<char> letters;
+  std::string::size_type pos = 0;
+  while ((pos = line.find_first_of(numbers, pos)) != std::string::npos) {
+    nums.emplace_back(line[pos]);
+    ++pos;
   }
-  std::ifstream inFile(argv[1]);
+  pos = 0;
+  while ((pos = line.find_first_not_of(numbers, pos)) != std::string::npos) {
+    letters.emplace_back(line[pos]);
+    ++pos;
+  }
+  nums.sort();
+  nums.unique();
+
+  std::cout << "found numbers: (";
+  for (const char c : nums) {
+    std::cout << c << ",";
+  }
+  std::cout << ")" << std::endl;
+  std::cout << "found letters: (";
+  for (const char c : letters) {
+    std::cout << c;
+  }
+  std::cout << ")" << std::endl;
+}
+// int main(int argc, char *argv[]) {
+int main() {
+  // if (argc < 2) {
+  //   std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
+  //   return 1;
+  // }
+  // std::ifstream inFile(argv[1]);
+  std::ifstream inFile("./text-files/input_add_item.txt");
   if (!inFile) {
     throw std::runtime_error("No input file found");
   }
@@ -37,7 +67,6 @@ int main(int argc, char *argv[]) {
   }
   std::cout << "\n-------END OF FILE-------\n"
             << "\nYour saved Values: " << std::endl;
-  ;
   for (const auto &l : lines) {
     std::cout << l << ", " << std::endl;
   }
@@ -45,5 +74,9 @@ int main(int argc, char *argv[]) {
   for (const auto &l : unsaved) {
     std::cout << l << ", " << std::endl;
   }
+  std::cout << std::endl;
+  std::string my_line = "T8a3nn34er i542s s343u245p2332e33r 43s323e87x97y";
+  std::string nums("0123456789");
+  find_nums(nums, my_line);
   return 0;
 }
