3b2d79cac9104fd7948485e9f5f17a7825d5f048
Tanner Davison <Tboydavison@gmail.com>
2025-03-23 21:44:30 -0600

    c

---
+++ chapters/9-containers-and-strings/adapters/stack/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+  std::priority_queue<std::string> strQue;
+  for (size_t i = 0; i < 10; ++i) {
+    int count = i;
+    strQue.emplace("String " + std::to_string(++count));
+  }
+  while (!strQue.empty()) {
+    std::cout << strQue.top() << ", ";
+    strQue.pop();
+  }
+
+  if (strQue.empty()) {
+    std::cout << "\nqueue is empty." << std::endl;
+  }
+  return 0;
+}
