d011441ea16810fe3d6afdd0fc76c1f6f2584fb3
Tanner Davison <Tboydavison@gmail.com>
2025-03-25 22:58:06 -0600

    my two iterations of a count_map slow/fast using stl algorithm techniques

---
+++ chapters/10-generic-algorithms/count_map/fast_count/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+int main() {
+  std::vector<int> nums{1, 2, 2, 2, 3, 4, 4, 4,  5, 5, 5, 6, 6,
+                        6, 7, 8, 9, 3, 2, 1, 23, 3, 2, 1, 2, 4};
+
+  std::unordered_map<int, int> count_map;
+
+  for (const auto &num : nums) {
+    count_map[num]++;
+  }
+  for (const auto &[number, count] : count_map) {
+    std::cout << number << " appears " << count << std::endl;
+    ;
+  }
+  return 0;
+};
---
+++ chapters/10-generic-algorithms/count_map/slow_count/main.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <utility>
+#include <vector>
+int main() {
+  std::vector<int> nums{1, 2, 2, 2, 3, 4, 4, 4,  5, 5, 5, 6, 6,
+                        6, 7, 8, 9, 3, 2, 1, 23, 3, 2, 1, 2, 4};
+  std::vector<std::pair<int, int>> num_pairs;
+
+  for (const auto &num : nums) {
+    int count_per_num = count(begin(nums), end(nums), num);
+    std::pair<int, int> curr_pair = std::make_pair(num, count_per_num);
+
+    if (std::find(begin(num_pairs), end(num_pairs), curr_pair) !=
+        num_pairs.end()) {
+      continue;
+    } else {
+      num_pairs.emplace_back(std::make_pair(num, count_per_num));
+    }
+  }
+  for (const auto &pair : num_pairs) {
+    std::cout << pair.first << " apears " << pair.second << " times."
+              << std::endl;
+  }
+  return 0;
+};
