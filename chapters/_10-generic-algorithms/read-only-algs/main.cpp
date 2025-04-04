18c45cc18e860b397a7ce1fdd0775272e73b3a46
Tanner Davison <Tboydavison@gmail.com>
2025-04-03 22:51:19 -0600

    gave an example using std algos for accumulate and equal

--- chapters/_10-generic-algorithms/read-only-algs/main.cpp
+++ chapters/_10-generic-algorithms/read-only-algs/main.cpp
@@ -1,6 +1,23 @@
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <map>
-#include <utility>
-
-int main() { return 0; }
+#include <numeric>
+#include <vector>
+// accumulate <int>
+// accumulate <string>
+//
+int main() {
+  std::vector<int> nums{20, 32, 44, 55, 66};
+  std::vector<int> nums_copy{20, 32, 44, 55, 66};
+  std::vector<std::string> strs{"Hello ", "world ", ", ", " how are you ",
+                                "today "};
+  std::cout << std::accumulate(nums.begin(), nums.end(), 0) << std::endl;
+  std::cout << std::accumulate(strs.cbegin(), strs.cend(), std::string(""));
+  if (std::equal(nums.cbegin(), nums.cend(), nums_copy.begin())) {
+    std::cout << "nums == nums_copy" << std::endl;
+  } else {
+    std::cout << "nums !== nums_copy"
+  }
+  return 0;
+}
