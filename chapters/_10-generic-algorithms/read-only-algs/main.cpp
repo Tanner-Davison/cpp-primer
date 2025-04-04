7318c893cd0a665917c740f76a21b0a29401dce3
Tanner Davison <Tboydavison@gmail.com>
2025-04-03 22:10:24 -0600

    moved to project

--- chapters/_10-generic-algorithms/read-only-algs/main.cpp
+++ chapters/_10-generic-algorithms/read-only-algs/main.cpp
@@ -3,40 +3,4 @@
 #include <map>
 #include <utility>
 
-int main() {
-
-  std::map<std::pair<int, int>, int> grid;
-  for (int i = 0; i <= 10; ++i) {
-
-    for (int j = 1; j <= 10; ++j) {
-      grid[std::make_pair(i, j)] = i;
-    }
-  }
-  int current_line = -1;
-  for (int i = 0; i < 11; ++i) {
-    if (i == 0) {
-      std::cout << std::setw(5) << "";
-      continue;
-    }
-    auto width = i > 1 ? std::setw(5) : std::setw(2);
-    std::cout << width << "  _" << i << "_";
-  }
-  std::cout << std::endl;
-  for (const auto &cell : grid) {
-    auto spacing = std::setw(2);
-    if (cell.first.first != current_line) {
-      if (current_line != -1) {
-        std::cout << std::endl;
-      }
-      current_line = cell.first.first;
-      std::cout << " _" << current_line << "_ ";
-    }
-    if (current_line == 10) {
-      spacing = std::setw(0);
-    }
-    std::cout << spacing << "(" << cell.first.first << "," << cell.first.second
-              << ") ";
-  }
-
-  return 0;
-}
+int main() { return 0; }
