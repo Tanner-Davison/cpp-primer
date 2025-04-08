8a63f1ba002dfb75b8da5d72d33343bdf102aae1
Tanner Davison <Tboydavison@gmail.com>
2025-04-07 23:14:41 -0600

    example of std::fill_n(std::back_inserter(ref), size, value);

--- chapters/_10-generic-algorithms/back_inserter/main.cpp
+++ chapters/_10-generic-algorithms/back_inserter/main.cpp
@@ -1,17 +1,28 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <vector>
 
 int main() {
+  // back inserter example
   std::vector<std::string> my_strs{"one", "two", "three", "four", "five"};
   auto it = std::back_inserter(my_strs);
   *it = "six";
   *it = "seven";
   *it = "eight";
-  // back inserter is used to add elements to the back of the container
-  // assaigned by a reference to the container
+  //
   for (const auto &str : my_strs) {
     std::cout << str << ", ";
+  }
+  std::cout << std::endl;
+
+  // using back_inserter as a way to fill containers
+  std::vector<int> my_ints;
+  std::fill_n(std::back_inserter(my_ints), 10, 5);
+  /*  - - - - - - - - - - - - - - - - - - - - - - -  */
+  std::cout << "using back_inserter to fill my_ints {}" << std::endl;
+  for (const auto &num : my_ints) {
+    std::cout << num << ",";
   }
   std::cout << std::endl;
   return 0;
