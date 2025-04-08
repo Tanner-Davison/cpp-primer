d238aca7d895dba4f20fceb20c3e745848a4afab
Tanner Davison <Tboydavison@gmail.com>
2025-04-07 23:18:16 -0600

    saved

--- chapters/_10-generic-algorithms/back_inserter/main.cpp
+++ chapters/_10-generic-algorithms/back_inserter/main.cpp
@@ -4,19 +4,19 @@
 #include <vector>
 
 int main() {
-  // back inserter example
+  /* std::back_inserter() example */
   std::vector<std::string> my_strs{"one", "two", "three", "four", "five"};
   auto it = std::back_inserter(my_strs);
   *it = "six";
   *it = "seven";
   *it = "eight";
-  //
+  /* - - - - - - - - - - - - - - - - - - */
   for (const auto &str : my_strs) {
     std::cout << str << ", ";
   }
   std::cout << std::endl;
 
-  // using back_inserter as a way to fill containers
+  /* using back_inserter as a way to fill containers */
   std::vector<int> my_ints;
   std::fill_n(std::back_inserter(my_ints), 10, 5);
   /*  - - - - - - - - - - - - - - - - - - - - - - -  */
