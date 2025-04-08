5c05b278cefef9628e9cd157dd3f4cfc95b7f812
Tanner Davison <Tboydavison@gmail.com>
2025-04-07 23:08:31 -0600

    added more examples

--- chapters/_10-generic-algorithms/back_inserter/main.cpp
+++ chapters/_10-generic-algorithms/back_inserter/main.cpp
@@ -6,6 +6,8 @@ int main() {
   std::vector<std::string> my_strs{"one", "two", "three", "four", "five"};
   auto it = std::back_inserter(my_strs);
   *it = "six";
+  *it = "seven";
+  *it = "eight";
   // back inserter is used to add elements to the back of the container
   // assaigned by a reference to the container
   for (const auto &str : my_strs) {
