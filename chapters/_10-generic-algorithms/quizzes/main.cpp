772e2119be28995a0cbe23204d7e20258436e28f
Tanner Davison <Tboydavison@gmail.com>
2025-04-08 21:35:33 -0600

    saving quizzes

---
+++ chapters/_10-generic-algorithms/quizzes/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::cout << "sequence before:" << std::endl;
+  std::vector<int> sequence{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  for (const auto &num : sequence) {
+    std::cout << num << ", ";
+  }
+  std::cout << std::endl;
+  std::cout << "sequence after std::fill_n:" << std::endl;
+  std::fill_n(begin(sequence), sequence.size(), 0);
+  for (const auto &num : sequence) {
+    std::cout << num << ", ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
