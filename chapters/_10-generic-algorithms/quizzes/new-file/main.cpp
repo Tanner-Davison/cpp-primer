0a72f79730c1ba27b2b68117983a8b86dca87421
Tanner Davison <tanner.davison95@gmail.com>
2025-07-11 23:21:54 -0600

    saving

---
+++ chapters/_10-generic-algorithms/quizzes/new-file/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Your Name
+ * All rights reserved.
+ *
+ */
+#include <iostream>
+int main() {
+  int x = 10;
+  int y = 100;
+  auto increment = [&, y] mutable {
+    x++;
+    y--;
+    std::cout << x;
+  };
+  for (std::size_t i = 0; i < 10; ++i) {
+    increment();
+    std::cout << std::endl;
+  }
+  std::cout << &x << std::endl;
+  std::cout << &y << std::endl;
+
+  return 0;
+}
