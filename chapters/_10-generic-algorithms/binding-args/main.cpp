ed00c62877615f3790780cfbb524f8075e7d58b1
Tanner Davison <Tboydavison@gmail.com>
2025-07-20 21:30:37 -0600

    algorithm using binding args in lambdas example code starter

--- chapters/_10-generic-algorithms/binding-args/main.cpp
+++ chapters/_10-generic-algorithms/binding-args/main.cpp
@@ -1,7 +1,19 @@
+#include <algorithm>
+#include <functional>
 #include <ios>
 #include <iostream>
 
+bool check_size(const std::string &s, std::string::size_type sz) {
+  return s.size() >= sz;
+}
 int main() {
   // auto newCallable = bind(callable, arg_list);
+  //
+  auto check6 = std::bind(check_size, std::placeholders::_1, 6);
+
+  std::string str = "Hello";
+  bool b1 = check6(str);
+  std::cout << "Hello is greater than 6 chars?" << std::boolalpha << b1
+            << std::endl;
   return 0;
 }
