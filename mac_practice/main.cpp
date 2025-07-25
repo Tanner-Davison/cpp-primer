27f756bc4b612d586f91fdf1e79769b85492861b
Tanner Davison <tanner.davison95@gmail.com>
2025-07-25 02:41:10 -0600

    practice done

--- mac_practice/main.cpp
+++ mac_practice/main.cpp
@@ -1,14 +1,21 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
-#include <vector>
+#include <string>
 int main() {
-  std::istream_iterator<int> iter_in(std::cin), eof;
-  std::ostream_iterator<int> iter_out(std::cout, "\n");
-  std::vector<int> container_for_inputs(iter_in, eof);
-  if (!container_for_inputs.empty()) {
-    for (; iter_in != eof; ++iter_in) {
-      iter_out = *iter_in;
-    }
+  std::ifstream in("./input.txt");
+  std::ofstream out_1("./output_1.txt"), out_2("./output_2.txt");
+  if (!in || !out_1 || !out_2) {
+    std::cout << "no input/or output files found" << std::endl;
+    return 0;
   }
-  return 0;
+  std::istream_iterator<std::string> iter_in(in), eof;
+  std::ostream_iterator<std::string> it_out_more(out_1, "\n");
+  std::ostream_iterator<std::string> it_out_less(out_2, "\n");
+  std::copy_if(iter_in, eof, it_out_more,
+               [&](std::string str) { return str.size() >= 7 == 1; });
+
+  std::copy_if(iter_in, eof, it_out_less,
+               [&](std::string str) { return str.size() <= 7 == 1; });
 }
