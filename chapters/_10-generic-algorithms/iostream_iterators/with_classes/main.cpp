b9f258acaed47e5543ef0ba5b1caaadfe0dc579f
Tanner Davison <Tboydavison@gmail.com>
2025-07-24 23:40:11 -0600

    added sort algorithm on transaction isbns read from files assaigned to the istream_iterators

--- chapters/_10-generic-algorithms/iostream_iterators/with_classes/main.cpp
+++ chapters/_10-generic-algorithms/iostream_iterators/with_classes/main.cpp
@@ -1,21 +1,38 @@
 #include "Sales_item.h"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <iterator>
+#include <vector>
 
 int main() {
-  std::istream_iterator<Sales_item> item_iter(std::cin), eof;
+  std::fstream transaction_list("./input.txt");
+  if (transaction_list) {
+    std::cout << "no input file found" << std::endl;
+  }
+  std::istream_iterator<Sales_item> item_iter(transaction_list), eof;
   std::ostream_iterator<Sales_item> out_iter(std::cout, "\n");
-
   Sales_item sum = *item_iter++;
+  std::vector<Sales_item> transactions;
+  ;
 
   while (item_iter != eof) {
     if (item_iter->isbn() == sum.isbn()) {
       sum += *item_iter++;
+      transactions.emplace_back(*item_iter);
     } else {
       out_iter = sum;
       sum = *item_iter++;
     }
   }
   out_iter = sum;
+
+  std::sort(transactions.begin(), transactions.end(),
+            [&](Sales_item &x, Sales_item &y) { return x.isbn() < y.isbn(); });
+  std::cout << "\n";
+
+  for (const auto &trans : transactions) {
+    std::cout << trans.isbn() << ", ";
+  }
   return 0;
 }
