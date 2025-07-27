29b6f67055fb0e2b6622e8b5bfdac7392ac29266
Tanner Davison <Tboydavison@gmail.com>
2025-07-27 00:27:33 -0600

    test complete

--- chapters/_10-generic-algorithms/quizzes/reverse-iters/main.cpp
+++ chapters/_10-generic-algorithms/quizzes/reverse-iters/main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 // list the 5 iterator categories:
-// -------------**----------------- \\
+//
 // 1.Input Iter
 // 2.Output Iter
 // 3.Forward Iter
@@ -12,29 +12,28 @@
 // 5.Random_access Iter
 //
 int main() {
+
   // use reverse iterators to print a vector in reverse order?
 
   std::ostream_iterator<int> print(std::cout, ", ");
   std::vector<int> num_vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  std::cout << "print in reverse from a vector using reverse iterators"
+  std::cout << "\n\nprint in reverse from a vector using reverse iterators"
             << std::endl;
   for (auto rev_it = num_vec.rbegin(); rev_it != num_vec.rend(); ++rev_it) {
     print = *rev_it;
   }
 
   // now print the elements in reverse order using ordinary iterators
+
   auto start = num_vec.begin();
   auto end = num_vec.end();
-  std::cout << "\n" << std::endl;
-
-  std::cout << "print a vector  in reverse using ordinary iterators:"
+  std::cout << "\n\nprint a vector  in reverse using ordinary iterators:"
             << std::endl;
   while (end != start) {
     end--;
     print = *end;
   }
 
-  std::cout << "\n" << std::endl;
   // given a vector that has ten elements, copy the elements from positions 3 -
   // 7 in reverse order to a list
 
@@ -42,9 +41,9 @@ int main() {
   auto revStart = num_vec.crbegin() + 3;
   auto revEnd = num_vec.crend() - 2;
   std::copy(revStart, revEnd, std::back_inserter(num_list));
-
-  std::cout << "copy elements from position 3-7 into a list in reverse order"
-            << std::endl;
+  std::cout
+      << "\n\ncopy elements from position 3-7 into a list in reverse order"
+      << std::endl;
   for (const auto &num : num_list) {
     std::cout << num << ", ";
   }
