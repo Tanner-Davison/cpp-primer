e9fa6a7c3ff136a601fc8970806a5c568c8f1b0f
Tanner Davison <Tboydavison@gmail.com>
2025-07-30 00:27:06 -0600

    implemented word map

--- chapters/_11_associative_containers/multiset/main.cpp
+++ chapters/_11_associative_containers/multiset/main.cpp
@@ -15,10 +15,11 @@ bool compareIsbn(const Sales_data &lhs, const Sales_data &rhs) {
 }
 
 int main() {
+
   std::vector<Sales_data> sales_vec(29);
   int gen_count = 0;
   std::generate(begin(sales_vec), end(sales_vec), [&gen_count]() {
-    gen_count += 5;
+    gen_count++;
     return Sales_data(gen_count);
   });
 
@@ -26,9 +27,25 @@ int main() {
       sales_vec.begin(), sales_vec.end(), compareIsbn);
 
   for (const auto &book : bookstore) {
-    std::cout << book.isbn << ", ";
+    if (book.isbn % 10 == 0) {
+      std::cout << "\n";
+      std::cout << book.isbn << ", ";
+    } else {
+
+      std::cout << book.isbn << ", ";
+    }
   }
   std::cout << std::endl;
 
+  // Searching the bookstore example
+  Sales_data target(17);
+  auto it = bookstore.find(target);
+  if (it != bookstore.end()) {
+    Sales_data found = *it;
+    std::cout << "\nTarget Found: " << found.isbn << std::endl;
+  } else {
+    std::cout << "Could not find: " << target.isbn
+              << " inside of the bookstore." << std::endl;
+  }
   return 0;
 }
