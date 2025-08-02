9bd718d7d19278be808d48e38f4e6a4760d3e716
Tanner Davison <Tboydavison@gmail.com>
2025-08-02 17:17:08 -0600

    formatted output

--- lesson-chapters/_11_associative_containers/quick-library-example/main.cpp
+++ lesson-chapters/_11_associative_containers/quick-library-example/main.cpp
@@ -1,24 +1,31 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <utility>
 #include <vector>
 
 int main() {
   std::multimap<std::string, std::vector<std::string>> library;
-  std::string author = "Tanner Davison";
+  auto tanners = std::make_pair(
+      "Tanner Davison",
+      std::vector<std::string>{"Dead of night", "Two cities", "no one returns ",
+                               "night of the cows "});
 
-  std::vector<std::string> tanners_books{
-      "Dead of night", "Two cities", "no one returns ", "night of the cows "};
+  std::sort(tanners.second.begin(), tanners.second.end());
+  library.insert(tanners);
 
-  std::sort(tanners_books.begin(), tanners_books.end());
-  library.insert({author, tanners_books});
-
-  auto tanners_inventory = library.find(author);
+  auto tanners_inventory = library.find(tanners.first);
   auto book_list = tanners_inventory->second;
+  std::cout << "[Author]:\t\t\t[Published Work]\n";
+  std::cout << "---------------------------------------------------\n";
 
-  for (const auto &book : book_list) {
-    std::cout << book << " [by: " << tanners_inventory->first << "]"
-              << std::endl;
+  for (const auto &[author, book_list] : library) {
+    std::cout << author << "\n";
+    int count = 0;
+    for (const auto &book : book_list) {
+      std::cout << "\t\t\t\t" << (++count) << ". " << book << "\n";
+    }
+    std::cout << "\n";
   }
   return 0;
 }
