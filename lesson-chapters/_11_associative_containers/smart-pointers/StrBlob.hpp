f0a1673500b077a69f780cb6897b3231eca42248
Tanner Davison <Tboydavison@gmail.com>
2025-08-10 21:58:56 -0600

    dynamic memory examples

--- lesson-chapters/_11_associative_containers/smart-pointers/StrBlob.hpp
+++ lesson-chapters/_11_associative_containers/smart-pointers/StrBlob.hpp
@@ -9,12 +9,15 @@ public:
   StrBlob(std::initializer_list<std::string> il);
   size_type size() const { return data->size(); }
   bool empty() const { return data->empty(); }
+
   // Add and remove elements
   void push_back(const std::string &t) { data->push_back(t); }
   long use_count() const { return data.use_count(); }
   void pop_back();
+
   std::string &front();
   std::string &back();
+  // Overloaded
   const std::string &back() const;
   const std::string &front() const;
 
--- lesson-chapters/_11_associative_containers/smart-pointers/main.cpp
+++ lesson-chapters/_11_associative_containers/smart-pointers/main.cpp
@@ -1,16 +1,49 @@
 #include "StrBlob.hpp"
 #include <iostream>
 
+int *create_int(const int num) {
+  int *nInt = new int(num);
+  return nInt;
+}
+
+void create_better_int(const int num) {
+  int *temp_num = new int(num);
+  std::cout << *temp_num << std::endl;
+  delete temp_num;
+}
+
 int main() {
   StrBlob blob({"sarah", "jackie", "bob"});
   StrBlob blob2 = blob;
+
+  // int* p1 = new int;    // default-initialized (garbage)
+  int *p2 = new int(); // value-initialized to 0
+  int *p3 = new int{}; // value-initialized to 0
+
+  auto pi = create_int(34);
+  std::cout << "Factory-created int: " << *pi << std::endl;
+
+  std::cout << "Better factory int: ";
+  create_better_int(1999);
+
+  // std::cout << *p1 << "\n";  // Would produce garbage
+  std::cout << "p2: " << *p2 << "\n";
+  std::cout << "p3: " << *p3 << "\n";
+
   if (!blob.empty()) {
-    std::cout << "\nFront: " << blob.front();
-    std::cout << "\nRef count[blob]: " << blob.use_count();
-    std::cout << "\nRef count[blob2]: " << blob2.use_count() << std::endl;
+    std::cout << "\nFront element: " << blob.front() << std::endl;
+    std::cout << "Ref count [blob]: " << blob.use_count() << std::endl;
+    std::cout << "Ref count [blob2]: " << blob2.use_count() << std::endl;
   } else {
     std::cout << "blob is empty" << std::endl;
   }
+
+  delete pi;
+  pi = nullptr;
+  std::cout << "pi after deletion: " << pi << std::endl;
+
+  delete p2;
+  delete p3;
 
   return 0;
 }
