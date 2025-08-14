0f2dc81080bda2657fd00eba1a2fca52ae49639f
Tanner Davison <tanner.davison95@gmail.com>
2025-08-13 20:36:24 -0600

    fixed formatting finally

--- lesson-chapters/_10_generic_algorithms/bind/binding-args/main.cpp
+++ lesson-chapters/_10_generic_algorithms/bind/binding-args/main.cpp
@@ -1,22 +1,23 @@
+// Copyright (c) 2025 Tanner Davison. All Rights Reserved.
 // implementation used in lambdas/lambda-dups-biggies
+#include <algorithm>
 #include <functional>
 #include <ios>
 #include <iostream>
 
 using namespace std::placeholders;
 
-bool check_size(const std::string &s, std::string::size_type sz) {
-  return s.size() >= sz;
+bool check_size(const std::string& s, std::string::size_type sz) {
+   return s.size() >= sz;
 }
 
 int main() {
-  // auto newCallable = bind(callable, arg_list);
+   // auto newCallable = bind(callable, arg_list);
+   auto check6 = bind(check_size, _1, 6);
+   auto str    = "Hello";
 
-  auto check6 = bind(check_size, _1, 6);
+   bool b1 = check6(str);
+   std::cout << "Hello is greater than 6 chars?" << std::boolalpha << b1 << std::endl;
 
-  std::string str = "Hello";
-  bool b1 = check6(str);
-  std::cout << "Hello is greater than 6 chars?" << std::boolalpha << b1
-            << std::endl;
-  return 0;
+   return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,1 +1,4 @@
-int a = 1;
+int x           = 5;
+int longname    = 10;
+double y        = 3.14;
+const char* str = "hello";
